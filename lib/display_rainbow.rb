ee266dda1acb450e7a617dfd83eb4569bd5996c5
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-10 19:44:50 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 letters = ["b","a","c"]
-def display_rainbow(letters)
+def display_rainbow(arg1)
   puts "The first letter in the alphabet is: #{letters[1]}"
   puts "The second letter in the alphabet is: #{letters[0]}"
   puts "The third letter in the alphabet is: #{letters[2]}"
 end
+display_rainbow(letters)
